daef7b9a1c0094b6ea752056da475045093b503d
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-25 11:25:38 +0530

    Some cosmetic changes to script.

--- scripts/add_moose_subtree.sh
+++ scripts/add_moose_subtree.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Adding master branch in subtree"
 (
-    git subtree add  --prefix moose-python https://github.com/BhallaLab/moose-python-package master --squash
-    git subtree add  --prefix moose-gui https://github.com/BhallaLab/moose-gui-package master --squash
+    git subtree add  --prefix moose-python \
+        https://github.com/BhallaLab/moose-python-package master --squash
+    git subtree add  --prefix moose-gui \
+        https://github.com/BhallaLab/moose-gui-package master --squash
 )
--- scripts/pull_moose_branch.sh
+++ scripts/pull_moose_branch.sh
@@ -3,6 +3,8 @@
 # NOTE: notice that we are using moose-python-package etc repositories.
 echo "Updating moose branches in subtree"
 (
-git subtree pull --prefix moose-python https://github.com/BhallaLab/moose-python-package master --squash
-git subtree pull --prefix moose-gui https://github.com/BhallaLab/moose-gui-package master --squash
+git subtree pull --prefix moose-python \
+    https://github.com/BhallaLab/moose-python-package master --squash
+git subtree pull --prefix moose-gui \
+    https://github.com/BhallaLab/moose-gui-package master --squash
 )
