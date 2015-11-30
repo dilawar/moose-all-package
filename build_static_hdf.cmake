d334944787b1bef346a95ff038c16c2cb8628fb9
Dilawar Singh <dilawars@ncbs.res.in>
2015-11-30 18:18:03 +0530

    Squashed 'moose-python/' changes from a3754d5..6a56cc6
    6a56cc6 Merge commit '7a189d603ff0626a1c6b7418aa1267f9e977841b'
    7a189d6 Squashed 'moose-core/' changes from effcaae..7d0e5f8
    f3f6466 Added hdf5 source code (stripped down version). If hdf is not available (centos), build locally.
    ae92e22 Added missing hl needed for nsdf.
    8cbc556 Merge branch 'master' of github.com:BhallaLab/moose-python-package
    09b3887 Added stripped down version hdf5 for insource compilation.
    
    git-subtree-dir: moose-python
    git-subtree-split: 6a56cc6609ab671b87f760078af664db38267d54

---
+++ build_static_hdf.cmake
@@ -0,0 +1,28 @@
+MESSAGE("++ Building local HDF5")
+
+SET(HDF5_SRC_DIR ${CMAKE_SOURCE_DIR}/dependencies/hdf5-1.8.16)
+SET(HDF5_INSTALL_DIR ${CMAKE_BINARY_DIR}/__hdf5_install)
+FILE(MAKE_DIRECTORY ${HDF5_INSTALL_DIR})
+MESSAGE("++ HDF5_SRC_DIR := ${HDF5_SRC_DIR}")
+
+SET(HDF5_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/__hdf5_build__)
+FILE(MAKE_DIRECTORY ${HDF5_BUILD_DIR})
+
+SET(HDF5_OUTPUT_BIN ${HDF5_INSTALL_DIR}/lib/libhdf5-static.a)
+ADD_CUSTOM_COMMAND(
+    OUTPUT ${HDF5_OUTPUT_BIN}
+    COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${HDF5_INSTALL_DIR} 
+        -DBUILD_SHARED_LIBRARY=OFF -DBUILD_STATIC_LIBRARY=ON 
+        ${HDF5_SRC_DIR}
+    COMMAND $(MAKE) 
+    COMMAND $(MAKE) install
+    WORKING_DIRECTORY ${HDF5_BUILD_DIR}
+    VERBATIM  # needed to handle escape characters.
+    )
+
+ADD_CUSTOM_TARGET(_libhdf5 ALL DEPENDS ${HDF5_OUTPUT_BIN})
+
+set(HDF5_INCLUDE_DIR ${HDF5_INSTALL_DIR}/include)
+
+## Ofcourse, moose-full depends on it.
+add_dependencies(_moose_static_dependencies _libhdf5)
