2607be8e15aab95844e455eb17f0a44b0e76d04d
Dilawar Singh <dilawars@ncbs.res.in>
2015-12-29 13:56:25 +0530

    Added debian dependencies.

--- moose_cpack.cmake
+++ moose_cpack.cmake
@@ -1,15 +1,23 @@
 # Packaging.
 INCLUDE(InstallRequiredSystemLibraries)
 SET(CPACK_SOURCE_STRIP_FILES ON)
+SET(CPACK_STRIP_FILES ON)
 SET(CPACK_SOURCE_IGNORE_FILES "${CPACK_SOURCE_IGNORE_FILES};*_build*")
-SET(CPACK_GENERATOR "DEB;RPM;STGZ;TGZ;TZ")
+SET(CPACK_GENERATOR "DEB;RPM;STGZ;TGZ")
 SET(CPACK_PACKAGE_NAME "moose-all")
 SET(CPACK_PACKAGE_VENDOR "Bhalla Lab, NCBS Bangalore")
+SET(CPACK_PACKAGE_CONTACT "Dilawar Singh <dilawars@ncbs.res.in>")
 SET(CPACK_PACKAGE_VERSION "3.0.2")
 SET(CPACK_PACKAGE_VERSION_MAJOR "3")
 SET(CPACK_PACKAGE_VERSION_MINOR "0")
 SET(CPACK_PACKAGE_VERSION_PATCH "2")
 SET(CPACK_SOURCE_GENERATOR "TGZ;TZ")
-SET(CPACK_PACKAGE_CONTACT "Dilawar Singh <dilawars@ncbs.res.in>")
+# THis is neccessary else we get /usr/usr, I don't know why. This fixes it. 
 SET(CPACK_PACKAGING_INSTALL_PREFIX /)
+SET(CPACK_WARN_ON_ABSOLUTE_INSTALL_DESTINATION ON)
+
+## Debian dependencies
+SET(CPACK_DEBIAN_PACKAGE_DEPENDS 
+    "python-numpy,python-matplotlib,python-lxml,python-nose,python-networkx,python-suds,python-qt4,python-sip,python-qt4-gl,libqt4-gui"
+    )
 include(CPack)
